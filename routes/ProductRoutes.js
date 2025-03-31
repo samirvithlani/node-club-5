1789a3d4c4386741bfe46eac71e89d1ad654d050
samirvithlani <samir.vithlani83955@gmail.com>
2025-03-31 17:20:09 +0530

    many

--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const productController = require('../controller/ProductController');
 router.post('/create', productController.createProduct);
 router.get('/getall', productController.getAllProducts);
+router.post("/addproducts",productController.createManyProduct)
 module.exports = router;
