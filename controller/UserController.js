f43dbeae4a94521887780d4ab457023e60da9cf6
samirvithlani <samir.vithlani83955@gmail.com>
2025-10-29 14:31:54 +0530

    user controller

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,14 +1,13 @@
 const userSchema = require("../model/UserSchema");
+const bcrypt = require("bcrypt")
 
 exports.filterUser = (req, res) => {
-
   //filter user data using query params
-  
 
   //console.log(name,email,age);
   //find data using ...query
   console.log(req.query);
-  userSchema.find({...req.query}, (err, data) => {
+  userSchema.find({ ...req.query }, (err, data) => {
     if (err) {
       res.status(500).json({
         message: "Error in fetching data",
@@ -17,7 +16,7 @@ exports.filterUser = (req, res) => {
       if (data.length === 0) {
         res.status(200).json({
           message: "No data found",
-          data:[]
+          data: [],
         });
       } else {
         res.status(200).json({
@@ -27,38 +26,31 @@ exports.filterUser = (req, res) => {
       }
     }
   });
-}
+};
 
-
-exports.filterUserData = async(req, res) => {
-
+exports.filterUserData = async (req, res) => {
   console.log(req.query);
-  try{
-  var users = await userSchema.find({...req.query})
-  if(users){
-    res.status(200).json({
-      message: "Data fetched successfully",
-      data: users,
-    });
-  }
-  else{
-    res.status(200).json({
-      message: "No data found",
-      data:[],
-    });
-  }
-  }catch(err){
+  try {
+    var users = await userSchema.find({ ...req.query });
+    if (users) {
+      res.status(200).json({
+        message: "Data fetched successfully",
+        data: users,
+      });
+    } else {
+      res.status(200).json({
+        message: "No data found",
+        data: [],
+      });
+    }
+  } catch (err) {
     res.status(500).json({
       message: "Error in fetching data",
     });
   }
-  
-
-}
-
+};
 
 exports.searchUserByName = (req, res) => {
-
   const name = req.params.name;
   userSchema.find({ name: { $regex: name, $options: "i" } }, (err, data) => {
     if (err) {
@@ -78,31 +70,44 @@ exports.searchUserByName = (req, res) => {
       }
     }
   });
-
-}
-exports.loginUser = (req, res) => {
-  userSchema.findOne(
-    { email: req.body.email, password: req.body.password },
-    (err, data) => {
-      if (err) {
-        res.status(500).json({
-          message: "Error in login",
-        });
-      } else {
-        if (data != null) {
-          res.status(200).json({
-            message: "Login successfully",
-            data: data,
-          });
-        } else {
-          res.status(404).json({
-            message: "Invalid credentials",
-          });
-        }
-      }
-    }
-  );
 };
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Step 1: Check if user exists by email
+    const user = await userSchema.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    // Step 2: Compare entered password with stored hashed password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    // Step 3: Login success
+    return res.status(200).json({
+      message: "Login successfully",
+      data: user,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      message: "Error in login",
+      error: err.message,
+    });
+  }
+};
+
 
 exports.getAllUsers = (req, res) => {
   userSchema.find((err, data) => {
@@ -133,19 +138,36 @@ exports.getAllUsers = (req, res) => {
 exports.createUser = (req, res) => {
   //object .... req.body....
   //console.log(req.body);
-  const user = new userSchema(req.body);
-  user.save((err, success) => {
-    if (err) {
-      res.status(500).json({
-        message: "Error in saving data",
-      });
-    } else {
-      res.status(201).json({
-        message: "Data saved successfully",
-        data: success,
-      });
+  
+  try {
+    const password = req.body.password;
+    if(password==undefined){
+      return res.json({
+        message:"password is required"
+      })
     }
-  });
+    
+    const hashedPassword = bcrypt.hashSync(req.body.password,10)
+    
+    const user = new userSchema({...req.body,password:hashedPassword});
+    user.save((err, success) => {
+      if (err) {
+        res.status(500).json({
+          message: "Error in saving data",
+        });
+      } else {
+        res.status(201).json({
+          message: "Data saved successfully",
+          data: success,
+        });
+      }
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "error while adding data",
+      err: err,
+    });
+  }
 };
 
 exports.getUserById = (req, res) => {
